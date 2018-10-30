87e2825a0f7fc90ddff5d6dbc69e95b5d29f92c0
user.email <nnp1997@gmail.com>
2018-10-30 15:26:02 -0400

    Second commit

---
+++ app/src/androidTest/java/edu/umd/cs/cmsc436/location_basedtourguide/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package edu.umd.cs.cmsc436.location_basedtourguide;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("edu.umd.cs.cmsc436.location_basedtourguide", appContext.getPackageName());
+    }
+}
