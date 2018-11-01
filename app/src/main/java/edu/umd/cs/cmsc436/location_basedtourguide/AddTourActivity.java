bc18b645bc7ad329b9c650da4125e677c23b12f6
yreiss1 <reiss.yuval@gmail.com>
2018-11-01 11:33:27 -0400

    Created AddTourActivity Component

--- app/src/main/java/edu/umd/cs/cmsc436/location_basedtourguide/AddTourActivity.java
+++ app/src/main/java/edu/umd/cs/cmsc436/location_basedtourguide/AddTourActivity.java
@@ -3,13 +3,11 @@ package edu.umd.cs.cmsc436.location_basedtourguide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AddTourActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_tour);
     }
-
-
 }
