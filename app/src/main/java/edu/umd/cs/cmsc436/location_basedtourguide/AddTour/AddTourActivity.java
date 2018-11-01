375ab942c23072ed1d71e3011020021111290f64
yreiss1 <reiss.yuval@gmail.com>
2018-11-01 11:42:34 -0400

    Updating proj structure

--- app/src/main/java/edu/umd/cs/cmsc436/location_basedtourguide/AddTour/AddTourActivity.java
+++ app/src/main/java/edu/umd/cs/cmsc436/location_basedtourguide/AddTour/AddTourActivity.java
@@ -1,7 +1,9 @@
-package edu.umd.cs.cmsc436.location_basedtourguide;
+package edu.umd.cs.cmsc436.location_basedtourguide.AddTour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import edu.umd.cs.cmsc436.location_basedtourguide.R;
 
 public class AddTourActivity extends AppCompatActivity {
 
