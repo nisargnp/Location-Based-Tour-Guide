8512f9d862baeea11125fd0da83193dfd888fb48
Christian <teeswizzle19@gmail.com>
2018-11-05 00:07:59 -0500

    Use encoded polylines instead of Lat/Lng for smoother paths

--- app/src/main/java/edu/umd/cs/cmsc436/location_basedtourguide/Util/Directions/DirectionsAsyncTask.java
+++ app/src/main/java/edu/umd/cs/cmsc436/location_basedtourguide/Util/Directions/DirectionsAsyncTask.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,11 +29,11 @@ import java.util.List;
  * Async Task for querying Google Directions API. Takes in a DirectionTaskParameter, and draws
  * the route between each waypoint on a GoogleMap
  */
-public class DirectionsAsyncTask extends AsyncTask<DirectionsTaskParameter, Void, List<LatLng>> {
+public class DirectionsAsyncTask extends AsyncTask<DirectionsTaskParameter, Void, List<String>> {
     private String TAG = "directions-async-task";
     private DirectionsTaskParameter directionsTaskParameter;
 
-    protected List<LatLng> doInBackground(DirectionsTaskParameter... params) {
+    protected List<String> doInBackground(DirectionsTaskParameter... params) {
         if (params.length < 1) {
             Log.i(TAG, "Directions Async Task received no parameters!");
             return null;
@@ -69,7 +70,7 @@ public class DirectionsAsyncTask extends AsyncTask<DirectionsTaskParameter, Void
         return parseJsonString(data);
     }
 
-    protected void onPostExecute(List<LatLng> result) {
+    protected void onPostExecute(List<String> result) {
         Log.i(TAG, "Directions Task Result:\n" + result);
 
         // add a poly line to the map using Lat/Lng coordinates
@@ -77,8 +78,9 @@ public class DirectionsAsyncTask extends AsyncTask<DirectionsTaskParameter, Void
         if (directionsTaskParameter != null && directionsTaskParameter.getMap() != null) {
             PolylineOptions polylineOptions = new PolylineOptions();
             for (int i = 0; i < result.size(); i++) {
-                LatLng endpoint = result.get(i);
-                polylineOptions.add(endpoint);
+                String encodedPolyline = result.get(i);
+                List<LatLng> stepPoints = PolyUtil.decode(encodedPolyline);
+                polylineOptions.addAll(stepPoints);
             }
             polylineOptions.color(Color.BLUE);
             directionsTaskParameter.getMap().addPolyline(polylineOptions);
@@ -109,16 +111,19 @@ public class DirectionsAsyncTask extends AsyncTask<DirectionsTaskParameter, Void
         return data.toString();
     }
 
-    private List<LatLng> parseJsonString(String data) {
+    /**
+     * Parse JSON Response and return a list of the encoded polyline for each step
+     * @param data
+     * @return list of strings representing encoded polylines
+     */
+    private List<String> parseJsonString(String data) {
         String ROUTES_TAG = "routes";
         String LEGS_TAG = "legs";
         String STEPS_TAG = "steps";
-        String START_LOCATION_TAG = "start_location";
-        String END_LOCATION_TAG = "end_location";
-        String LAT_TAG = "lat";
-        String LNG_TAG = "lng";
+        String POLYLINE_TAG = "polyline";
+        String POINTS_TAG = "points";
 
-        List<LatLng> result = new ArrayList<>();
+        List<String> result = new ArrayList<>();
 
 //            Log.i(TAG, "Parsing data: " + data);
 
@@ -139,13 +144,9 @@ public class DirectionsAsyncTask extends AsyncTask<DirectionsTaskParameter, Void
 
                     for (int steps_idx = 0; steps_idx < steps.length(); steps_idx++) {
                         JSONObject stepObject = steps.getJSONObject(steps_idx);
-                        JSONObject startLocation = stepObject.getJSONObject(START_LOCATION_TAG);
-                        JSONObject endLocation = stepObject.getJSONObject(END_LOCATION_TAG);
+                        JSONObject polylineObject = stepObject.getJSONObject(POLYLINE_TAG);
 
-                        result.add(new LatLng(startLocation.getDouble(LAT_TAG),
-                                startLocation.getDouble(LNG_TAG)));
-                        result.add(new LatLng(endLocation.getDouble(LAT_TAG),
-                                endLocation.getDouble(LNG_TAG)));
+                        result.add(polylineObject.getString(POINTS_TAG));
                     }
                 }
             }
