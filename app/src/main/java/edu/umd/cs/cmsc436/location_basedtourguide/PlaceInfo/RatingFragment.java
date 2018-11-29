cfcec31b5afdee622ad3668097d1873389e66dd8
nisargnp <e4f95ad145fb4e9ceebbc0a828086382>
2018-11-28 20:31:47 -0500

    Enable commenting

--- app/src/main/java/edu/umd/cs/cmsc436/location_basedtourguide/PlaceInfo/RatingFragment.java
+++ app/src/main/java/edu/umd/cs/cmsc436/location_basedtourguide/PlaceInfo/RatingFragment.java
@@ -12,13 +12,20 @@ import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import edu.umd.cs.cmsc436.location_basedtourguide.Data.DataStore.DataStore;
+import edu.umd.cs.cmsc436.location_basedtourguide.Firebase.DTO.Comment;
+import edu.umd.cs.cmsc436.location_basedtourguide.Firebase.DTO.Tour;
+import edu.umd.cs.cmsc436.location_basedtourguide.Firebase.DTO.User;
+import edu.umd.cs.cmsc436.location_basedtourguide.Firebase.Utils.FirebaseUtils;
 import edu.umd.cs.cmsc436.location_basedtourguide.R;
 
 public class RatingFragment extends DialogFragment {
     TextView ratingText;
     TextView commentDesc;
     TextView nameDesc;
-    RatingBar rating;
+    RatingBar ratingBar;
     Button submitButton;
     EditText commentBox;
     EditText nameText;
@@ -40,7 +47,7 @@ public class RatingFragment extends DialogFragment {
         builder.setView(host);
 
         ratingText = host.findViewById(R.id.textView);
-        rating = host.findViewById(R.id.ratingbar);
+        ratingBar = host.findViewById(R.id.ratingbar);
         submitButton = host.findViewById(R.id.submit_button);
         commentDesc = host.findViewById(R.id.comment_desc);
         commentDesc.setText("Add a comment:");
@@ -51,14 +58,46 @@ public class RatingFragment extends DialogFragment {
         nameText = host.findViewById(R.id.name_box);
         nameText.setHint("Enter name (optional)");
         ratingText.setText("Rate Tour!");
-        rating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
 
+        ratingBar.setStepSize(1);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             // Called when the user swipes the RatingBar
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
                 ratingText.setText(getString(R.string.rating_string, rating));
             }
         });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int rating = (int) ratingBar.getRating();
+                String commentStr = commentBox.getText().toString();
+                String nameStr = nameText.getText().toString();
+
+                User user = new User();
+                user.setName(nameStr);
+                String userId = FirebaseUtils.uploadToFirebase(getActivity(), user, null);
+
+                Comment comment = new Comment();
+                comment.setText(commentStr);
+                comment.setAuthor(userId);
+                String commentId = FirebaseUtils.uploadToFirebase(getActivity(), comment, null);
+
+                Tour tour = DataStore.getInstance().getTour(tourId);
+                tour.setRating(tour.getRating() + rating);
+                tour.setNumVotes(tour.getNumVotes() + 1);
+                ArrayList<String> comments = new ArrayList<>(tour.getComments());
+                comments.add(commentId);
+                tour.setComments(comments);
+                FirebaseUtils.updateFirebaseRaw(tour.getId(), tour);
+
+                dismiss();
+
+            }
+        });
+
         return builder.create();
     }
 
